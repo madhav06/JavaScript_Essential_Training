db286648ebf193ee170d76f967d49171ec32947b
madhav06 <mnandan06@gmail.com>
2021-09-04 00:16:45 +0530

    Var Global Scope Variable

--- 06_03/script.js
+++ 06_03/script.js
@@ -9,13 +9,14 @@ document.querySelector(".left").style.backgroundColor = color;
 document.querySelector(".left .color-value").innerHTML = color;
 
 color = "skyblue";
+function headingColor() {
+    color = "blue";
+    document.querySelector(".title").style.color = color;
+  }
+  
 
 document.querySelector(".right").style.backgroundColor = color;
 document.querySelector(".right .color-value").innerHTML = color;
 
-function headingColor() {
-  color = "blue";
-  document.querySelector(".title").style.color = color;
-}
-
 headingColor();
+
