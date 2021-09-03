fa5c4a3a46d5bbe0c4f2a4f234247fbe5bcff966
madhav06 <mnandan06@gmail.com>
2021-09-03 23:58:48 +0530

    Variables

--- 06_02/script.js
+++ 06_02/script.js
@@ -2,3 +2,8 @@
  * The var statement
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/var
  */
+
+ var person = "John Doe", carName = "Volvo", price = "32000"
+ 
+price = Number(price)
+console.log(`${"$ "}${price}`)
