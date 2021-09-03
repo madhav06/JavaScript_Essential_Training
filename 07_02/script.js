5f56c3a4f9fb20920f5cfb2def2ef8ef6a8a0dc8
madhav06 <mnandan06@gmail.com>
2021-09-04 01:05:59 +0530

    Arrays Basics

---
+++ 07_02/script.js
@@ -0,0 +1,20 @@
+/**
+ * Working with arrays
+ * @link https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Arrays
+ */
+
+let item = "flashlight";
+
+const collection = ["Piggy", item, 5, true];
+
+collection[2] = "camera"
+collection[collection.length] = "new item"
+collection[9] = "at the end"
+
+console.log(collection);
+console.log(collection.length);
+console.log(collection[1]);
+console.log(collection[8]);
+
+
+
