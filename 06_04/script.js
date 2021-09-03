e1a34fb32ae2c9841818b14dd47a272f3a9fb144
madhav06 <mnandan06@gmail.com>
2021-09-04 00:25:06 +0530

    let: Local Scope Variable

--- 06_04/script.js
+++ 06_04/script.js
@@ -3,7 +3,7 @@
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/let
  */
 
-var color = "purple";
+let color = "purple";
 
 document.querySelector(".left").style.backgroundColor = color;
 document.querySelector(".left .color-value").innerHTML = color;
@@ -11,11 +11,16 @@ document.querySelector(".left .color-value").innerHTML = color;
 color = "skyblue";
 
 function headingColor() {
-  let color = "blue";
-  document.querySelector(".title").style.color = color;
+  let titleColor = "blue";
+  document.querySelector(".title").style.color = titleColor;
+  console.log("inside: ", titleColor);
 }
 
 headingColor();
 
+console.log("outside: ", titleColor);
 document.querySelector(".right").style.backgroundColor = color;
 document.querySelector(".right .color-value").innerHTML = color;
+// for most of situation when we want to changable or mutable variable,
+// we use " let that include we can also use them locally, globally".
+// if we want immutable variable with global scope all the time, use " var ".
