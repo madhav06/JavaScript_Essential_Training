f600f94eba1f4e66f5e8e5b6357986e38b6a97fd
madhav06 <mnandan06@gmail.com>
2021-09-04 00:52:03 +0530

    Arithmatic operators

---
+++ 06_08/script.js
@@ -0,0 +1,22 @@
+/**
+ * Arithmetic operators
+ * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators#Arithmetic_operators
+ */
+
+let a = 15;
+let b = 4;
+let c = 3.2;
+
+console.log(`let a: ${a} (${typeof a})`);
+console.log(`let b: ${b} (${typeof b})`);
+console.log(`let c: ${c} (${typeof c})`);
+
+let result = (a * b) / c;
+let modulores = a % b
+let powerofres = a ** b
+
+console.log("Result: ", a++)
+console.log(a)
+console.log("Result Compute: ", result);
+console.log("Result Modulo: ", modulores);
+console.log("Result Power oF: ", powerofres);
